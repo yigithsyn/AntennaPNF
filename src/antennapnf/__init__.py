db49fb358996d53b3584814ec809ba8c471a45ea
Hüseyin YİĞİT <yigit.hsyn@gmail.com>
2026-03-13 16:53:53 +0300

    13.03: add functions

--- src/antennapnf/__init__.py
+++ src/antennapnf/__init__.py
@@ -1,46 +1,154 @@
-# from . import AntennaPNF as AntennaPNF
+from math import atan2, degrees, tan, radians
 
 C0 = 299792458  # m/s
 
 
 class Utility:
 
-    def freq2wlen(freq: float) -> float:
+    @staticmethod
+    def frequencyToWavelength(freq: float) -> float:
         return C0 / freq
 
 
 class AntennaPNF:
 
-    def __init__():
-        pass
+    @staticmethod
+    def seperationDistance(frequency: float, coeff: float) -> float:
+        r"""Seperation distance in terms of vavelength multiples for near-field antenna measurements in meters
 
-    def seperationDistance(coeff: float, frequency: float) -> float:
-        r"""Seperation distance respect to wavelength coefficient for near-field antenna measurements in meters
+        
+        Parameters
+        ----------
+        coeff : float 
+            multiplication coefficient 
+        frequency : float
+            frequency of interest in Hertz [Hz]
 
+        Returns
+        -------
+        float
+            seperation distance in meters [m]
+
+        Formula
+        -------
         .. math::
-        R_{nf} = k\times\lambda = k\times c_0/f
-
-        :param float coeff: multiplication coefficient 
-        :param float frequency: frequency of interest in Hertz [Hz]
-        :return float: distance n meters [m]
+            R_{nf} = k\times\lambda = k\times c_0/f
         """
-        return coeff * Utility.freq2wlen(frequency)
+        return coeff*Utility.frequencyToWavelength(frequency)
 
 
-
+    @staticmethod
     def minSeperationDistance(frequency: float, three_lambda: bool = False) -> float:
-        r"""Minimum recommended seperation distance between antena and probe aperture for near-field antenna measurements in meters
+        r""" Minimum recommended seperation distance between antenna and probe for near-field antenna measurements
 
-        .. math::R_{nf} = 5\times\lambda = 5\times c_0/f
+        Parameters
+        ----------
+        frequency : float
+            frequency of interest in Hertz [Hz]
 
+        closer : bool
+            use 3 lambda criteria as minimum, defaults to False
+
+        Returns
+        -------
+        float
+            seperation distance in meters [m]
+
+        Notes
+        -----
         Standard suggests to choose between 3 or 5 wavelength.
         In order to ensure copuling effect, 5 wavelength distance is choosen and implemented. 3 wavelength is optional.
 
+        References
+        ----------
         .. [1] IEEE 1720-2012 Recommended Practice for Near-Field Antenna Measurements, Section 5.3.1.4, Page 27.
 
-        :param float frequency: frequency of interest in Hertz [Hz]
-        :param bool three_lambda: use 3 lambda criteria as minimum, defaults to False
-        :return float: minimum distance in meters [m]
+        Formula
+        -------
+        .. math::
+            R_{nf} = 5\times\lambda = 5\times c_0/f
         """
-        return 3*Utility.freq2wlen(frequency) if three_lambda else 3*Utility.freq2wlen(frequency)
+        return 3*Utility.frequencyToWavelength(frequency) if three_lambda else 5*Utility.frequencyToWavelength(frequency)
+    
+
+    @staticmethod
+    def angleOfView(a: float, d: float, L: float) -> float:
+        r""" Reliable far-field angle-of-view in planar near-field antenna measurements
+
+        Parameters
+        ----------
+        a : float
+            antenna cross-section length [m]
+        d : float
+            seperation distance between antenna and probe [m]
+        L : float
+            scan length of region [m]
+
+        Notes
+        -----
+        Calculation assumes a scanning region centered on the AUT.  
+
+        References
+        ----------
+        .. [1] IEEE 149-2021 Recommended Practice for Antenna Measurements, Section 12.5, Page 135, Equation 99.
+        .. [2] IEEE 1720-2012 Recommended Practice for Near-Field Antenna Measurements, Section 5.3.1.6, Page 28, Equation 27.
+
+        Formula
+        -------
+        .. math::
+            theta = \atan^{-1} \left \dfrac{L-a}{2d} \right
+        """
+        return degrees(atan2(L-a, 2*d))
+    
+    @staticmethod
+    def samplingSpacing(frequency: float):
+        r""" Maximum sampling length for near-field antenna measurements
+
+        Parameters
+        ----------
+        frequency : float
+            frequency of interest in Hertz [Hz]
+
+        References
+        -----
+        .. [1] IEEE 149-2021 Recommended Practice for Antenna Measurements, Section 12.5, Page 135.
+        .. [2] IEEE 1720-2012 Recommended Practice for Near-Field Antenna Measurements, Section 5.2.5, Page 23, Equation 25.
+
+        Formula
+        -------
+        .. math::
+            \Delta = \lambda / 2 = 0.5 \times (c_0 / f)
+        """
+        return 0.5*Utility.frequencyToWavelength(frequency)
+    
+    @staticmethod
+    def scanLength(a: float, d: float, theta:float):
+        r""" Required length of the scan for desired angle-of-view in planar near-field antenna measurements
+
+        Parameters
+        ----------
+        a     : float
+                antenna cross-section length [m]
+        d     : float
+                seperation distance between antenna and probe [m]
+        theta : float
+                desired pattern view angle along one side [deg]
+
+        Notes
+        -----
+        Calculation assumes a scanning region centered on the AUT.
+        Input distances are unitless so output is the same quantity of inputs.
+
+        References
+        ----------
+        .. [1] IEEE 149-2021 Recommended Practice for Antenna Measurements, Section 12.5, Page 135, Equation 99.
+        .. [2] IEEE 1720-2012 Recommended Practice for Near-Field Antenna Measurements, Section 5.3.1.6, Page 28, Equation 27.
+
+        Formula
+        -------
+        .. math::
+            L = 2d \cdot \tan\theta + a
+        """
+        
+        return 2*d*tan(radians(theta))+a
 
--- tests/__main__.py
+++ tests/__main__.py
@@ -5,11 +5,54 @@ sys.path.append('./src/')
 
 from antennapnf import AntennaPNF
 
+def test_seperationDistance():
+    frequency = 10E9
+    coeff = 10
+
+    print()
+    assert round(AntennaPNF.seperationDistance(frequency, coeff),3) == pytest.approx(0.3)
+    print(f"- Seperation distance at {frequency/1E6}MHz is {AntennaPNF.seperationDistance(frequency,coeff):.3f} meters with coefficient={coeff}")
+
+
 def test_minSeperationDistance():
     frequency = 3E9
 
-    assert round(AntennaPNF.minSeperationDistance(frequency),3) == pytest.approx(0.3)
-    print(f"- Minimum seperation distance at {frequency/1E6}MHz is {AntennaPNF.minSeperationDistance(frequency):.3f}")
-    # assert print(AntennaPNF.minSeperationDistance(frequency))
+    print()
+    assert round(AntennaPNF.minSeperationDistance(frequency),3) == pytest.approx(0.5)
+    print(f"- Minimum seperation distance at {frequency/1E6}MHz is {AntennaPNF.minSeperationDistance(frequency):.3f} meters")
+    assert round(AntennaPNF.minSeperationDistance(frequency, True),3) == pytest.approx(0.3)
+    print(f"- Minimum seperation distance at {frequency/1E6}MHz is {AntennaPNF.minSeperationDistance(frequency, True):.3f} meters (3 lambda crtieria)")
+
+
+def test_angleOfView():
+    a = 0.15
+    d = 0.05
+    L = 1.00
+
+    result = 83.290
+
+    print()
+    assert round(AntennaPNF.angleOfView(a,d,L),3) == pytest.approx(result)
+    print(f"- Angle of view (d={a}, d={d}, L={L}) is {AntennaPNF.angleOfView(a,d,L):.3f}")
+
+def test_samplingSpacing():
+    freq = 5E9
+
+    result = 0.030
+
+    print()
+    assert round(AntennaPNF.samplingSpacing(freq),3) == pytest.approx(result)
+    print(f"- Sampling spacing (frequency={freq/1e6}MHz) is {AntennaPNF.samplingSpacing(freq):.3f}")
+
+def test_scanLength():
+    a = 0.15
+    d = 0.05
+    theta = 83.290
+
+    result = 1.000
+
+    print()
+    assert round(AntennaPNF.scanLength(a,d,theta),3) == pytest.approx(result)
+    print(f"- Scan length (d={a}, d={d}, theta={theta}) is {AntennaPNF.scanLength(a,d,theta):.3f}")
 
 
